d88e85c45c20fadcc1aa07d56768f8fe89f517e1
BiscayRobin <biscay.rob@gmail.com>
2021-09-02 21:48:43 +0200

    Add: Added options to cli and brought argp to the table.
    This change allow for the definition of a CLI that is more complex.
    Compilation option has been added because it should be the most optimised method to execute brainfuck.

--- src/arguments.h
+++ src/arguments.h
@@ -1,9 +1,13 @@
 #ifndef __ARGUMENTS_H
 #define __ARGUMENTS_H
 
+#include <stdbool.h>
+
 typedef struct
 {
     char *in_file;
+    char *out_file;
+    bool isCompiled;
 } args_t;
 
 int argument_parse(int argc, char **argv, args_t *args_result);
